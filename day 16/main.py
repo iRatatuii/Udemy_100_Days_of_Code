21715ff09427f4534c9f46ef810b21d8d472b063
iRatatuii <andreikarpenkin123@gmail.com>
2024-09-24 14:36:37 +0300

    day 16

--- day 16/main.py
+++ day 16/main.py
@@ -1,3 +1,22 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+while True:
+    user_choice = input(f'What would you like? {menu.get_items()}:')
+    if user_choice == 'off':
+        break
+    elif user_choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+        continue
+    else:
+        drink = menu.find_drink(user_choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
