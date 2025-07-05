5ae3b9845b82c16e1c28b24ed898ac460ba1e1ef
iratatui <andreikarpenkin123@gmail.com>
2025-07-05 07:58:16 +0300

    add day 17

---
+++ day 17/quiz_game/main.py
@@ -0,0 +1,19 @@
+from data import question_data
+from question_module import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for item in question_data:
+    text = item.get('text')
+    answer = item.get('answer')
+    question_bank.append(Question(text, answer))
+
+
+quiz = QuizBrain(question_list=question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+
+print("You've completed the quiz")
+print(f"Your final score was {quiz.score}/{len(quiz.question_list)}")
